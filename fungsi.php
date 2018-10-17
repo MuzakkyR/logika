3bf2425e43a931b3652e955400d1620a22aa63b3
MuzakkyR <muzakky_r@outlook.com>
2018-10-17 16:13:46 +0700

    update

--- fungsi.php
+++ fungsi.php
@@ -30,4 +30,49 @@
         $itung = array_map('strlen', $meledak);
         echo max($itung);
     }
+
+    function piramid($tinggi){
+        for($x = 1; $x <= $tinggi; $x++){
+            for($y = $tinggi; $y > $x; $y--){
+                echo "_";
+            }
+            for($z = 0; $z < $x*2-1; $z++){
+                echo "*";
+            }
+            for($v = $tinggi; $v > $x; $v--){
+                echo "_";
+            }
+            echo "<br>";
+        }
+    }
+    function revpiramid($tinggi){
+        for($x = $tinggi; $x >= 1; $x--){
+            for($y=$tinggi ; $y > $x; $y--){
+                echo"_";
+            }
+            for($z = 0; $z < $x*2-1; $z++){
+                echo "*";
+            }
+            for($v=$tinggi ; $v > $x; $v--){
+                echo"_";
+            }
+            echo "<br>";
+        }
+    }
+    function l_piramid($tinggi){
+        for($x = $tinggi; $x>=1; $x--){
+            for($z = $x; $z >= 1; $z-- ){
+                echo "*";
+            }
+            echo "<br>";
+        }
+    }
+    function r_piramid($tinggi){
+        for($x = 1; $x<=$tinggi; $x++){
+            for($z = 1; $z <= $x; $z++ ){
+                echo "*";
+            }
+            echo "<br>";
+        }
+    }
 ?>
--- index.php
+++ index.php
@@ -9,62 +9,104 @@ include "fungsi.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    <div class="fibonacci" style="margin:20px; padding:20px;">
-    <h2>Fibonacci</h2>
-    <form action="index.php" method="get">
-        Masukkan jumlah bilangan fibonacci
-        <input type="number" name="input_fibo">
-        <input type="submit" name="fibo" value="Submit">
-    </form>
-    <?php
-        if(isset($_GET['fibo']) AND $_GET['input_fibo'] != null){
+    <div class="fungsi">
+        <h2>Fibonacci</h2>
+        <form action="index.php" method="get">
+            Masukkan jumlah bilangan fibonacci
+            <input type="number" name="input_fibo">
+            <input type="submit" name="btn_fibo" value="Submit">
+        </form>
+        <?php
+            if(isset($_GET['btn_fibo']) AND $_GET['input_fibo'] != null){
 
-            $jumlah = $_GET['input_fibo'] - 2;
-            if($jumlah >= 0){
-                fibonacci($jumlah);
-            } else{
-                echo "Input harus lebih dari 1";
-            }
+                $jumlah = $_GET['input_fibo'] - 2;
+                if($jumlah >= 0){
+                    fibonacci($jumlah);
+                } else{
+                    echo "Input harus lebih dari 1";
+                }
 
-        } elseif(isset($_GET['fibo']) AND $_GET['input_fibo'] == null){
+            } elseif(isset($_GET['btn_fibo']) AND $_GET['input_fibo'] == null){
+                
+                echo "isi kolom dengan angka";
             
-            echo "isi kolom dengan angka";
-        
-        }
-    ?>   
+            }
+        ?>   
     </div>
   
-    <div class="reversekalimat" style="margin:20px; padding:20px;">
+    <div class="fungsi">
         <h2>Membalik Kalimat</h2>
         <form action="index.php" method="get">
             Masukkan Kalimat
             <input type="text" name="kalimat">
-            <input type="submit" value="Submit" name="reverse">
+            <input type="submit" value="Submit" name="btn_reverse">
         </form>
         <?php
-            if(isset($_GET['reverse']) AND $_GET['kalimat'] != null){
+            if(isset($_GET['btn_reverse']) AND $_GET['kalimat'] != null){
                 $normal = $_GET['kalimat'];
                revpalindrom($normal);
-            } elseif(isset($_GET['reverse']) AND $_GET['kalimat'] != null){
+            } elseif(isset($_GET['btn_reverse']) AND $_GET['kalimat'] != null){
                 echo "isi kolom dengan kalimat atau kata";
             }
         ?>
     </div>
 
-    <div class="binarygap" style="margin:20px; padding:20px;">
+    <div class="fungsi">
         <h2>Menghitup gap biner</h2>
         <form action="index.php" method="get">
             Masukkan angka
             <input type="number" name="input_biner" id="">
-            <input type="submit" value="Submit" name="biner">
+            <input type="submit" value="Submit" name="btn_biner">
         </form>
 
         <?php
-            if(isset($_GET['biner']) AND $_GET['input_biner']){
+            if(isset($_GET['btn_biner']) AND $_GET['input_biner']){
                 $angka = $_GET['input_biner'];
                 binergap($angka);
+            }
+        ?>
+    </div>
+
+    <div class="fungsi">
+        <h2>Membuat Piramid</h2>
+        <form action="index.php" method="get">
+            Masukkan Tinggi
+            <input type="number" name="input_piramid">
+            <input type="submit" value="Submit" name="btn_piramid">            
+        </form>
+        <div class="tampil_piramid">
+            <?php
+                if(isset($_GET['btn_piramid']) AND $_GET['input_piramid']){
+                    $tinggi = $_GET['input_piramid'];
+                    echo"<div class='satu'>";
+                        piramid($tinggi);
+                    echo"</div>";
+                    echo"<div class='satu'>";
+                        revpiramid($tinggi);
+                    echo"</div>";
+                    echo"<div class='satu'>";
+                        l_piramid($tinggi);
+                    echo"</div>";
+                    echo"<div class='satu'>";
+                        r_piramid($tinggi);
+                    echo"</div>";
+                }
+            ?>
+        </div>
+    </div>
+
+    <div class="fungsi">
+        <h2>Menghitung Bunga</h2>
+        <input type="number" name="input_saldo">
+        <input type="number" name="input_bunga">
+        <input type="number" name="input_periode">
+        <input type="submit" value="Hitung" name="btn_bunga">
+        <?php
+            if(isset($_GET['btn_bunga']) AND $_GET['input_saldo']){
+                
             }
         ?>
     </div>
